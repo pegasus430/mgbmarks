5502fae0fdb2653e3cfc78db623c0f01e5f88629
softcomet <realseniordev210@gmail.com>
2018-11-25 20:53:48 -0500

    add marcador app and update model

---
+++ marcador/models.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.timezone import now
+
+
+@python_2_unicode_compatible
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class Bookmark(models.Model):
+    url = models.URLField()
+    title = models.CharField('title', max_length=255)
+    description = models.TextField('description', blank=True)
+    is_public = models.BooleanField('public', default=True)
+    date_created = models.DateTimeField('date created')
+    date_updated = models.DateTimeField('date updated')
+    owner = models.ForeignKey(User, verbose_name='owner',
+        related_name='bookmarks')
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    class Meta:
+        verbose_name = 'bookmark'
+        verbose_name_plural = 'bookmarks'
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return '%s (%s)' % (self.title, self.url)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.date_created = now()
+        self.date_updated = now()
+        super(Bookmark, self).save(*args, **kwargs)
